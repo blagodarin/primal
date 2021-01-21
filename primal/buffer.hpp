325317edcd38a2a7f80302eacf187ffe94255ada
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-01-22 00:30:29 +0300

    Refactor Buffer tests

--- primal/buffer.hpp
+++ primal/buffer.hpp
@@ -8,7 +8,6 @@
 
 #include <cassert>
 #include <memory>
-#include <type_traits>
 
 namespace primal
 {
--- tests/buffer.cpp
+++ tests/buffer.cpp
@@ -5,6 +5,9 @@
 #include <primal/buffer.hpp>
 #include <primal/buffer.hpp>
 
+#include <limits>
+#include <numeric>
+
 #include <doctest.h>
 
 TEST_CASE("Buffer::Buffer()")
@@ -14,17 +17,18 @@ TEST_CASE("Buffer::Buffer()")
 	CHECK(buffer.size() == 0);
 }
 
-TEST_CASE("Buffer::Buffer(size_t)")
+TEST_CASE("Buffer::Buffer(3)")
 {
-	primal::Buffer<int> buffer{ 2 };
+	primal::Buffer<int> buffer{ 3 };
 	REQUIRE(buffer.data());
-	CHECK(buffer.size() == 2);
-	buffer[0] = 1;
-	buffer[1] = 2;
-	CHECK(buffer.data()[0] == 1);
-	CHECK(buffer.data()[1] == 2);
-	buffer[0] = 3;
-	buffer[1] = 4;
-	CHECK(buffer.data()[0] == 3);
-	CHECK(buffer.data()[1] == 4);
+	CHECK(buffer.size() == 3);
+	std::iota(buffer.data(), buffer.data() + buffer.size(), 1);
+	CHECK(buffer[0] == 1);
+	CHECK(buffer[1] == 2);
+	CHECK(buffer[2] == 3);
+}
+
+TEST_CASE("Buffer::Buffer(SIZE_MAX)")
+{
+	CHECK_THROWS_AS(primal::Buffer<int> buffer{ std::numeric_limits<size_t>::max() }, std::bad_alloc);
 }
