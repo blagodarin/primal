c080b76020d401bb573395ae003e62923e0f249f
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-01-21 20:28:09 +0300

    Implement aligned allocation support

--- primal/allocator.hpp
+++ primal/allocator.hpp
@@ -2,6 +2,7 @@
 // Copyright (C) Sergei Blagodarin.
 // SPDX-License-Identifier: Apache-2.0
 
+#include <bit>
 #include <cstdlib>
 #include <new>
 
@@ -20,6 +21,35 @@ namespace primal
 		static void deallocate(void* memory) noexcept
 		{
 			std::free(memory);
+		}
+	};
+
+	template <size_t kAlignment>
+	class AlignedAllocator
+	{
+	public:
+		static_assert(std::has_single_bit(kAlignment));
+
+		static void* allocate(size_t size)
+		{
+			constexpr auto kAlignmentMask = kAlignment - 1;
+			const auto alignedSize = (size + kAlignmentMask) & ~kAlignmentMask;
+#ifdef _MSC_VER
+			if (const auto memory = ::_aligned_malloc(alignedSize, kAlignment); memory)
+#else
+			if (const auto memory = std::aligned_alloc(kAlignment, alignedSize); memory)
+#endif
+				[[likely]] return memory;
+			throw std::bad_alloc{};
+		}
+
+		static void deallocate(void* memory) noexcept
+		{
+#ifdef _MSC_VER
+			::_aligned_free(memory);
+#else
+			std::free(memory);
+#endif
 		}
 	};
 }
--- tests/allocator.cpp
+++ tests/allocator.cpp
@@ -17,10 +17,32 @@ namespace
 			primal::Allocator::deallocate(pointer);
 		}
 	};
+
+	constexpr size_t kAlignment = 256;
+
+	using AlignedAllocator = primal::AlignedAllocator<kAlignment>;
+
+	struct AlignedAllocatorDeleter
+	{
+		void operator()(void* pointer) noexcept
+		{
+			AlignedAllocator::deallocate(pointer);
+		}
+	};
 }
 
 TEST_CASE("Allocator::allocate()")
 {
 	std::unique_ptr<void, AllocatorDeleter> pointer{ primal::Allocator::allocate(1) };
 	CHECK(pointer);
+	const auto misalignment = std::bit_cast<uintptr_t>(pointer.get()) % kAlignment;
+	MESSAGE("Default allocator misalignment: ", misalignment, " % ", kAlignment);
+	CHECK(misalignment != 0);
+}
+
+TEST_CASE("AlignedAllocator::allocate()")
+{
+	std::unique_ptr<void, AlignedAllocatorDeleter> pointer{ AlignedAllocator::allocate(1) };
+	CHECK(pointer);
+	CHECK(std::bit_cast<uintptr_t>(pointer.get()) % kAlignment == 0);
 }
